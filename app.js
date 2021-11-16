ca85acb016a44d828f8cf0092e587adba6c570be
akinkayagithub <akinkaya.b@gmail.com>
2021-11-15 20:30:16 -0600

    added middleware setup

--- app.js
+++ app.js
@@ -1,19 +1,37 @@
 //Require Libraries
 const express = require('express');
+const exphbs = require('express-handlebars');
 
 //App Setup
 const app = express();
 
 //Middleware
+// app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+
+var hbs = exphbs.create({
+    defaultLayout: 'main',
+});
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 //Routes
 app.get('/', (req, res) => {
-    res.send('Hello Squirrel');
+    // get url of gif
+    const gifUrl = 'https://media1.tenor.com/images/561c988433b8d71d378c9ccb4b719b6c/tenor.gif?itemid=10058245';
+
+    // res.send('Hello Squirrel');
+    res.render('hello-gif', {gifUrl});
+});
+
+
+app.get('/greetings/:name/', (req, res) => {
+    const name = req.params.name;
+    res.render('greetings', {name});
 });
 
 //Start server
 const port = 3000;
 
 app.listen(port, () =>{
-    console.log('Giffy working');
+    console.log(`Giffy App listening on port localhost: ${port}!`);
 });
