5815d859a76cafa658a5ccab82feb593ccd1208c
akinkayagithub <akinkaya.b@gmail.com>
2021-11-25 10:23:16 -0600

    added styles

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const hbs = exphbs.create({
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
-
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     // Handle the home page when we haven't queried yet
